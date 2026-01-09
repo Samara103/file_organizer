30e671bbdf0400c952cfbbee1315f4d63b3ae787
Samara <samara52075@gmail.com>
2026-01-09 08:09:48 -0600

    Day 4 updates

---
+++ src/organizer/dsa_day4.py
@@ -0,0 +1,48 @@
+def second_largest(nums):
+    '''
+    find the second largest number.
+
+    Parameter:
+        nums (list): list of numbers
+    '''
+    pass
+
+
+def has_duplicates(nums):
+    '''
+    return true if list has a duplicates.
+
+    Parameters:
+        nums (list): list of numners
+    '''
+    pass
+
+
+def pair_sums(nums):
+    '''
+    return the sum of every pair of adjacent numbers.
+
+    Parameters:
+        nums (list): list of numbers
+    '''
+    pass
+
+
+def longest_increasing(nums):
+    '''
+    find the longest increating sequence.
+
+    Parameters: 
+        nums (list): list of numbers
+    '''
+    pass
+
+
+def max_window_sum(nums):
+    '''
+    sliding window: max sum of any window of size k
+
+    Parameters:
+        nums (list): list of numbers
+    '''
+    pass
